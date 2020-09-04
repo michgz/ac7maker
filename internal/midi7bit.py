b7204bd2791c230bbb6024380619767c62a26000
michael <mdr.nz11@gmail.com>
2020-09-04 18:23:08 +0100

    Add a function for uploading AC7 files over MIDI

---
+++ internal/midi7bit.py
@@ -0,0 +1,127 @@
+##
+#
+# Functions for encoding and decoding MIDI-style 7-bit byte strings.
+#
+
+import struct
+
+
+
+def midi_7bit_to_8bit(b):
+
+  r = 0  # remainder
+  n = 0  # position of split
+  i = 0  # pointer to input
+  c = b'' # output
+  
+  
+  while i<len(b):
+
+    x = b[i]
+  
+    if x >= 128:
+      print("Not valid 7-bit data at position {0} : {1:02X}!".format(i, x))
+      raise Exception
+
+    if n == 0 or n == 8:
+      r = x
+      n = 0
+    elif n == 1:
+      c += struct.pack('<B', ((x&0x01)<<7) + r)
+      r = x//2
+    elif n == 2:
+      c += struct.pack('<B', ((x&0x03)<<6) + r)
+      r = x//4
+    elif n == 3:
+      c += struct.pack('<B', ((x&0x07)<<5) + r)
+      r = x//8
+    elif n == 4:
+      c += struct.pack('<B', ((x&0x0f)<<4) + r)
+      r = x//16
+    elif n == 5:
+      c += struct.pack('<B', ((x&0x1f)<<3) + r)
+      r = x//32
+    elif n == 6:
+      c += struct.pack('<B', ((x&0x3f)<<2) + r)
+      r = x//64
+    elif n == 7:
+      c += struct.pack('<B', ((x&0x7f)<<1) + r)
+      r = 0
+    i += 1
+    n += 1
+  if r != 0:
+    print("Left over data! Probably an error")
+    raise Exception
+    
+  return c
+  
+  
+  
+
+
+
+def midi_8bit_to_7bit(b):
+  r = 0  # remainder
+  n = 0  # position of split
+  i = 0  # pointer to input
+  c = b'' # output
+  
+  while i<len(b):
+    if n == 0 or n == 7:
+      n = 0
+      c += struct.pack('<B', 0x1*(b[i] & 0x7f))
+      r = (b[i] & 0x80)//0x80
+    elif n == 1:
+      c += struct.pack('<B', r + 0x2*(b[i] & 0x3f))
+      r = (b[i] & 0xc0)//0x40
+    elif n == 2:
+      c += struct.pack('<B', r + 0x4*(b[i] & 0x1f))
+      r = (b[i] & 0xe0)//0x20
+    elif n == 3:
+      c += struct.pack('<B', r + 0x8*(b[i] & 0x0f))
+      r = (b[i] & 0xf0)//0x10
+    elif n == 4:
+      c += struct.pack('<B', r + 0x10*(b[i] & 0x07))
+      r = (b[i] & 0xf8)//0x8
+    elif n == 5:
+      c += struct.pack('<B', r + 0x20*(b[i] & 0x03))
+      r = (b[i] & 0xfc)//0x4
+    elif n == 6:
+      c += struct.pack('<B', r + 0x40*(b[i] & 0x01))
+      c += struct.pack('<B', (b[i] & 0xfe)//0x2)
+      r = 0
+    n += 1
+    i += 1
+  if n < 7:
+    c += struct.pack('<B', r)
+  return c
+  
+
+
+
+if __name__=="__main__":
+  
+  # Unit tests
+
+  test_in = b'\x41\x43\x30\x37\x58\x5F\x00\x00\x1C\x00\x00' 
+  test_out = midi_8bit_to_7bit(test_in)
+
+  s = ''
+  for t in test_out:
+    s += "{0:02X} ".format(t)
+    
+  print(s)
+
+
+
+
+
+  test_in = b'\x41\x06\x41\x39\x03\x6B\x17\x00\x00\x38\x00\x00'
+  test_out = midi_7bit_to_8bit(test_in)
+
+  s = ''
+  for t in test_out:
+    s += "{0:02X} ".format(t)
+    
+  print(s)
+
---
+++ sysex_comms.py
@@ -0,0 +1,270 @@
+#! /bin/usr/python3
+
+##
+#
+# A library of functions for communicating with a CT-X3000 keyboard over
+# the MIDI interface. Linux only. Assumes that there is only one MIDI
+# connection, i.e. the device will be located at "/dev/midi1".
+#
+# Casio keyboards other than CT-X3000 may also work with some of the
+# functions, but none have been tested.
+#
+#
+## Dependencies:
+#
+# Needs crcmod. Install using:
+#
+#     pip3 install crcmod
+#
+#
+## Functions:
+#
+#   upload_ac7(dest_num, data)
+#   =====================
+#
+# Uploads an AC7 rhythm to the keyboard. Parameters:
+#
+#   dest_num:  Number of the destination. Must lie in the user area, i.e.
+#               values 294-343 inclusive.
+#   data:      Byte array to write to the destination. It is in "HBR"
+#               format, which is identical to the format of an .AC7 file
+#               as saved by the keyboard. It should be possible to just
+#               read an .AC7 file and upload it with no modification.
+#
+# Example:
+#
+#    with open("MyRhythm.AC7", "rb") as f:
+#      x = f.read()
+#    upload_ac7(294, x)
+#
+#
+
+import time
+import os
+import struct
+import sys
+import crcmod
+
+
+
+from internal.midi7bit import midi_7bit_to_8bit
+from internal.midi7bit import midi_8bit_to_7bit
+
+
+# Define the Linux device name. Assumes this is the only MIDI connection
+DEVICE_NAME = "/dev/midi1"
+
+# Define the device ID. Constructed as follows:
+#    0x44       Manufacturer ID ( = Casio)
+#    0x19 0x01  Model ID ( = CT-X3000 or CT-X5000)
+#    0x7F       Device. This is a "don't care" value
+#
+DEVICE_ID = b"\x44\x19\x01\x7F"
+
+
+
+
+
+
+is_busy = False
+must_send_ack = False
+have_got_ack = False
+
+so_far = b''
+
+
+def handle_pkt(p):
+  global is_busy
+  global must_send_ack
+  global have_got_ack
+  if len(p) < 7:
+    print("BAD PACKET!!")
+    return
+  if p[0] != 0xF0 or p[1] != 0x44 or p[4] != 0x7F or p[-1] != 0xF7:
+    print("BAD PACKET!!")
+    return
+  type_of_pkt = p[5]
+  if type_of_pkt == 0xB:
+    is_busy = True
+  else:
+    is_busy = False
+    if type_of_pkt == 0xA:
+      have_got_ack = True
+    
+    
+  if type_of_pkt == 3 or type_of_pkt == 5:   # This takes a CRC
+    crc = crcmod.predefined.PredefinedCrc('crc-32')
+    crc.update(p[1:-6])
+    c = struct.unpack('<5B', p[-6:-1])
+    crc_compare = c[0] + (1<<7)*c[1] + (1<<14)*c[2] + (1<<21)*c[3] + (1<<28)*c[4]
+    if crc.crcValue == crc_compare:
+      must_send_ack = True
+    else:
+      print("BAD CRC!!!")
+
+
+
+def parse_response(b):
+  global so_far
+  
+  in_pkt = True
+  if len(so_far) == 0:
+    in_pkt = False
+  
+  for i in range(len(b)):
+    x = b[i]
+    if in_pkt:
+      if x == 0xF7:
+        so_far += b'\xf7'
+        # Have completed. Do something!
+        handle_pkt(so_far)
+        in_pkt = False
+        so_far = b''
+      elif x == 0xF0:
+        # Error! but start a new packet
+        so_far = b'\xf0'
+      elif x >= 0x80:
+        # Error!
+        in_pkt = False
+        so_far = b''
+      else:
+        so_far += b[i:i+1]
+    else:
+      if x == 0xF0:
+        so_far = b'\xf0'
+        in_pkt = True
+
+
+def wait_for_ack(f):
+  global have_got_ack
+  have_got_ack = False
+  st = time.monotonic()
+  while True:
+    parse_response(os.read(f, 20))
+    if have_got_ack:
+      # Success!
+      return
+    time.sleep(0.2)
+    if time.monotonic() > st + 4.0:
+      # Timed out. Completely exit the program
+      print("SYSEX communication timed out. Exiting ...")
+      sys.exit(0)
+
+
+def make_packet(tx=False,
+                category=30,
+                memory=1,
+                parameter_set=0,
+                block=[0,0,0,0],
+                parameter=0,
+                index=0,
+                length=1,
+                command=-1,
+                sub_command = 3,
+                data=b''):
+
+
+  w = b'\xf0' + DEVICE_ID
+  if command < 0:
+    if (tx):
+      command = 1
+    else:
+      command = 0
+  w += struct.pack('<B', command)
+  if command == 0xA:
+    return w + b'\x00\x00\x00\x00\xf7'
+  if command == 0x8:
+    return w + struct.pack('<B', sub_command) + b'\xf7'
+
+  w += struct.pack('<2B', category, memory)
+  w += struct.pack('<2B', parameter_set%128, parameter_set//128)
+  
+  if command == 5:
+    w += struct.pack('<2B', length%128, length//128)
+    w += midi_8bit_to_7bit(data)
+    crc = crcmod.predefined.PredefinedCrc('crc-32')
+    crc.update(w[1:])
+    w += midi_8bit_to_7bit(struct.pack('<I', crc.crcValue))
+    w += b'\xf7'
+    return w
+    
+  elif (command >= 2 and command < 8) or command == 0xD or command == 0xE: # OBR/HBR doesn't have the following stuff
+
+    pass
+
+    
+  else:
+    if len(block) != 4:
+      print("Length of block should be 4, was {0}; setting to all zeros".format(len(block)))
+      block = [0,0,0,0]
+    for blk_x in block:
+      w += struct.pack('<H', 0x7F7F & blk_x)
+    w += struct.pack('<3H', parameter, index, length-1)
+  if (tx):
+    w += data
+  w += b'\xf7'
+  return w
+
+
+
+def upload_ac7(dest_num, data):
+
+  if dest_num < 294 or dest_num > 343:
+    print("Incorrect destination number")
+    raise Exception
+  pp = dest_num - 294
+
+
+  # Open the device
+  f = os.open(DEVICE_NAME, os.O_RDWR)
+
+
+  # Flush the input queue
+  parse_response(os.read(f, 20))
+  time.sleep(0.4)
+
+
+  # Send the SBS command
+  pkt = make_packet(command = 8, sub_command = 3)
+  #print(pkt)
+  os.write(f, pkt)  # SBS(HBS)
+  wait_for_ack(f)
+
+
+  i = 0
+  while i < len(data):
+    # Send a HBS packet:
+    # Category 30 = Rhythms
+    # Parameter set: indicates the specific rhythm
+    # Memory 1 = user rhythm space
+    
+    len_remaining = len(data) - i
+    if len_remaining > 0x80:
+      len_remaining = 0x80 
+    
+    
+    pkt = make_packet(parameter_set=pp, category=30, memory=1, command=5, length=len_remaining, data = data[i:i+len_remaining])
+    #print(pkt)
+    os.write(f, pkt)
+    wait_for_ack(f)
+    i += len_remaining
+
+
+
+  # Send ESS (no ACK expected)
+  #print("Sending ESS")
+  os.write(f, make_packet(parameter_set=pp, category=30, memory=1, command=0xd))
+  time.sleep(0.3)
+
+  # Send EBS (no ACK expected)
+  #print("Sending EBS")
+  os.write(f, make_packet(parameter_set=pp, category=30, memory=1, command=0xe))
+  time.sleep(0.3)
+
+  os.close(f)
+
+
+
+if __name__=="__main__":
+  pass
+
