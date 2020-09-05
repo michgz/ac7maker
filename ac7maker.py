ee4eb60dde1f42040a002cf38582ef20a37bfe41
michael <mdr.nz11@gmail.com>
2020-09-05 13:48:40 +0100

    Rejig it all quite a bit

--- ac7maker.py
+++ ac7maker.py
@@ -4,13 +4,14 @@
 # Import some standard modules
 import json
 import struct
-import operator
+#import operator
+import sys
 
 # Import one non-standard module. Can be installed by:
 #
 #   pip3 install mido
 #
-import mido
+#import mido
 
 
 # Default instrument allocations. Ordered by part number
@@ -30,32 +31,33 @@ instruments = [
 # but here just give the elements the same values.
 def ac7make_mixer_element(pt, b):
   g = b''
-  pt_patch = b['rhythm']['parts'][pt].get('patch', -1)
-  pt_bank = b['rhythm']['parts'][pt].get('bank_msb', -1)
+  pt_patch = b['rhythm']['parts'][pt-1].get('patch', -1)
+  pt_bank = b['rhythm']['parts'][pt-1].get('bank_msb', -1)
   if pt_patch < 0 or pt_bank < 0:
     # Not specified correctly - use defaults for that part
-    pt_patch = instruments[pt]['patch']
-    pt_bank = instruments[pt]['bank_msb']
+    pt_patch = instruments[pt-1]['patch']
+    pt_bank = instruments[pt-1]['bank_msb']
   g = g + struct.pack('B', pt_patch)
   g = g + struct.pack('B', pt_bank)
-  g = g + struct.pack('B', b['rhythm']['parts'][pt].get('volume', 100))  # volume
-  g = g + struct.pack('B', 64 + b['rhythm']['parts'][pt].get('pan', 0))   # pan
-  g = g + struct.pack('B', b['rhythm']['parts'][pt].get('reverb_send', 40))   # reverb send
-  g = g + struct.pack('B', b['rhythm']['parts'][pt].get('chorus_send', 0))   # chorus send
+  g = g + struct.pack('B', b['rhythm']['parts'][pt-1].get('volume', 100))  # volume
+  g = g + struct.pack('B', 64 + b['rhythm']['parts'][pt-1].get('pan', 0))   # pan
+  g = g + struct.pack('B', b['rhythm']['parts'][pt-1].get('reverb_send', 40))   # reverb send
+  g = g + struct.pack('B', b['rhythm']['parts'][pt-1].get('chorus_send', 0))   # chorus send
   return(g)
 
-def ac7make_mixer(b, addr):
+def ac7make_mixer(mixers, addr):
   g1 = b'MIXR'
   g2 = b''
   g3 = b''
-  number_of_parts = 96
+  number_of_parts = len(mixers)
+  if number_of_parts != 96:
+    print("Expected 96 mixer parts, got {0}".format(number_of_parts))
+    raise Exception
   addr = addr + 10 + 4*number_of_parts
-  for el in range(12):
-    for pt in range(8):
-      g4 = ac7make_mixer_element(pt, b)
-      g3 = g3 + g4
-      g2 = g2 + struct.pack('<I', addr)
-      addr = addr + len(g4)
+  for mm in mixers:
+    g3 = g3 + mm
+    g2 = g2 + struct.pack('<I', addr)
+    addr = addr + len(mm)
   g1 = g1 + struct.pack('<I', 10 + len(g2) + len(g3))
   g1 = g1 + struct.pack('<H', number_of_parts)
   return (g1 + g2 + g3)
@@ -108,26 +110,148 @@ def ac7make_drum_element(pt, el, b):
   # 00 FC 00   -- end of track  
   return b'\x80\xff\x04\x00\xfc\x00'
 
-def ac7make_drum(b, addr):
+def ac7make_drum(drums, start_addr):
   g1 = b'DRUM'
   g2 = b''
   g3 = b''
   
-  number_of_parts = 0
-  for el in range(12):
-    if ac7make_element_has_drum(b, el):
-      number_of_parts += 2
-  addr = addr + 10 + 4*number_of_parts
-  for el in range(12):
-    if ac7make_element_has_drum(b, el):
-      for pt in range(2):
-        g4 = ac7make_drum_element(pt, el, b)
-        g3 = g3 + g4
-        g2 = g2 + struct.pack('<I', addr)
-        addr = addr + len(g4)
+  number_of_parts = len(drums)
+  addr = start_addr + 10 + 4*number_of_parts
+  for dd in drums:
+    g3 += dd
+    g2 = g2 + struct.pack('<I', addr)
+    addr += len(dd)
   g1 = g1 + struct.pack('<I', 10 + len(g2) + len(g3))
   g1 = g1 + struct.pack('<H', number_of_parts)
   return (g1 + g2 + g3)
+
+# Table of possible specifications for "conversion_table". 16 possibilities
+# are given, corresponding to integers 0-15. In fact, integers up to
+# 255 are accepted by the keyboard, but most of them give glitchy results
+# and are probably of no use.
+conversions = [
+  "Bass Basic",
+  "Bass 7th",
+  "Basic",
+  "Var2",
+  "Var3",
+  "Var4",
+  "7th",
+  "Minor",
+  "Phrase",
+  "Bass Minor",
+  "Penta",
+  "Intro n-minor",
+  "Intro m-minor",
+  "Intro h-minor",
+  "Intro no Change",
+  "Intro dorian"
+]
+
+# Table of possible specifications for "inversion". 8 possibilities are
+# given, corresponding to integers 0-7. Only 3 are documented, the 5 
+# other integer values are accepted by the keyboard and give potentially
+# interesting results.
+# TODO: add a way to select the undocumented values.
+inversions = [
+  "Off",
+  "",
+  "On",
+  "",
+  "7th",
+  "",
+  "",
+  ""
+]
+
+
+def ac7make_chord_conversion(tk):
+  cconv = tk.get("conversion_table", "")
+  x = -1
+  if cconv != "":
+    try:
+      x = conversions.index(cconv)
+    except ValueError:
+      x = -1
+  
+  # If a conversion has been specified, return it.
+  if x >= 0:
+    return x
+
+  # No conversion has been specified. Choose a default based on
+  # part and element.
+  pt_1 = tk["part"]
+  el_1 = tk["element"]
+  if (el_1 == 1 or el_1 == 6 or el_1 == 7 or el_1 == 12):
+    # An intro or outro element
+    if (pt_1 == 3):
+      # Bass
+      return conversions.index("Intro n-minor")
+    else:
+      # Chords
+      return conversions.index("Intro n-minor")
+  else:
+    # An ordinary variation/fill
+    if (pt_1 == 3):
+      # Bass
+      return conversions.index("Bass Basic")
+    else:
+      # Chords
+      return conversions.index("Basic")
+
+
+def ac7make_chord_inversion(tk):
+  cinv = tk.get("inversion", "")
+  x = -1
+  if cinv != "":
+    try:
+      x = inversions.index(cinv)
+    except ValueError:
+      x = -1
+  
+  # If an inversion has been specified, return it.
+  if x >= 0:
+    return x
+
+  # No inversion has been specified. Choose a default value
+  return inversions.index("Off")
+
+def ac7make_break_point(tk):
+  bkp = tk.get("break_point", -1)
+  
+  # If a break point has been specified, return it.
+  if bkp >= 0 and bkp <= 12:
+    return bkp
+
+  # No break point has been specified. Choose a default value
+  pt_1 = tk["part"]
+  if (pt_1 == 3):
+    # Bass
+    return 4
+  else:
+    # Chords
+    return 7
+
+
+def ac7make_starter(tk):
+  # Make up the "starter" (first 3 bytes) of a track.
+  
+  if tk["part"] <= 2:
+    # This is a drum part. No starter.
+    return b''
+    
+  x1 = ac7make_chord_conversion(tk)
+  x2 = (ac7make_break_point(tk) << 4)                   \
+                + (ac7make_chord_inversion(tk) << 1)    \
+                + 0x1&tk.get("retrigger", 0)
+  
+  x3 = 0x7F&tk.get("lowest_note", 0) + (tk.get("f-root", 0)<<7)
+  return struct.pack("<3B", x1, x2, x3)
+
+
+def ac7make_track_data(tk):
+  # TODO: read MIDI data for the track
+  return b''
 
 
 def ac7make_other_element(pt, el, b):
@@ -157,105 +281,25 @@ def ac7make_other_element(pt, el, b):
       return b'\x02\x70\x00\x00\xe5\x00\x00\x24\x42\x00\x2b\x53\x00\x30\x47\x00\x34\x3e\x00\x37\x49\x00\x3c\x3a\x00\x34\x3e\x00\x30\x47\x00\x2b\x53\x8a\x37\x00\x01\x2b\x00\x00\x30\x00\x00\x3c\x00\x00\x30\x00\x00\x2b\x00\x02\x34\x00\x00\x34\x00\x01\x24\x00\x2f\x2b\x4f\x01\x37\x44\x01\x30\x4a\x00\x24\x47\x00\x3c\x3d\x00\x34\x3f\x1f\x30\x00\x01\x2b\x00\x01\x37\x00\x01\x24\x00\x00\x3c\x00\x02\x34\x00\x35\x24\x36\x00\x2b\x4c\x01\x37\x44\x00\x34\x3f\x00\x3c\x35\x00\x30\x50\x38\x2b\x00\x00\x37\x00\x00\x30\x00\x01\x34\x00\x01\x3c\x00\x01\x24\x00\x2c\xfc\x00'
   return g1 + b'\x80\xff\x04\x00\xfc\x00'
 
-def ac7make_other(b, addr):
+
+def ac7make_other(others, start_addr):
   g1 = b'OTHR'
   g2 = b''
   g3 = b''
   
-  number_of_parts = 0
-  for el in range(12):
-    if ac7make_element_has_other(b, el):
-      number_of_parts += 6
-  addr = addr + 10 + 4*number_of_parts
-  for el in range(12):
-    if ac7make_element_has_other(b, el):
-      for pt in range(6):
-        g4 = ac7make_other_element(pt, el, b)
-        g3 = g3 + g4
-        g2 = g2 + struct.pack('<I', addr)
-        addr = addr + len(g4)
+  number_of_parts = len(others)
+  addr = start_addr + 10 + 4*number_of_parts
+  for oo in others:
+    g3 = g3 + oo
+    g2 = g2 + struct.pack('<I', addr)
+    addr = addr + len(oo)
   g1 = g1 + struct.pack('<I', 10 + len(g2) + len(g3))
   g1 = g1 + struct.pack('<H', number_of_parts)
   return (g1 + g2 + g3)
 
 
-
-drum_part_number = 0
-other_part_number = 0
-overall_part_number = 0
-
 def ac7make_element_atom(in_val, in_str):
   return struct.pack('B', in_val) + struct.pack('B', len(in_str)) + in_str
-
-def ac7make_element_block(b, el):
-  g1 = b'ELMT'
-  g2 = b''
-  g2 = g2 + ac7make_element_atom(1, struct.pack('B', 34)) # ??
-  measure_count = 1
-  if el == 6 or el == 11:
-    measure_count = 4
-  g2 = g2 + ac7make_element_atom(6, struct.pack('B', measure_count))   # Number of measures
-  
-  global drum_part_number
-  global other_part_number
-  global overall_part_number
-  
-  g32 = b''
-  
-  drum_part_count = 0
-  if ac7make_element_has_drum(b, el):
-    drum_part_count = 2
-  
-  other_part_count = 0
-  if ac7make_element_has_other(b, el):
-    other_part_count = 6
-
-  
-  g2 += ac7make_element_atom(7, struct.pack('B', drum_part_count + other_part_count))   # Number of parts
-  
-  for i in range(drum_part_count):
-    g32 += struct.pack('B', drum_part_number)
-    g32 += b'\x80'  # Flags byte
-    drum_part_number += 1
-
-  for i in range(other_part_count):
-    g32 += struct.pack('B', other_part_number)
-    g32 += b'\x80'  # Flags byte
-    other_part_number += 1
-  
-  g33 = b''
-  
-  for i in range(8):
-    if i < (drum_part_count + other_part_count):
-      g33 += struct.pack('B', overall_part_number)
-      g33 += b'\x80'  # Flags byte
-    # This one increments even if the parts aren't included
-    overall_part_number += 1
-
-
-  g34 = b''
-  if (drum_part_count + other_part_count) > 0:
-    g34 = b'\x0f'  # 0x0F = negative 1
-    for i in range(drum_part_count + other_part_count - 1):
-      g34 += struct.pack('B', i)
-    
-  g48 = b''
-  for i in range(8):  # This one always has 8 elements
-    g48 += struct.pack('B',  b['rhythm']['parts'][i].get('delay_send', 0))   # delay send
-
-
-  g2 += ac7make_element_atom(32, g32) # Data offsets
-  g2 += ac7make_element_atom(33, g33) # Mixer offsets
-  g2 += ac7make_element_atom(34, g34) #??
-  g2 += ac7make_element_atom(48, g48) # Delay send values
-
-  g2 = g2 + ac7make_element_atom(253, b'') #??
-  g2 = g2 + ac7make_element_atom(254, b'') #??
-  g2 = g2 + ac7make_element_atom(255, b'') # End
-  
-  g1 = g1 + struct.pack('<H', 6 + len(g2)) 
-  return g1 + g2
-
 
 
 def ac7make_time_signature(s):
@@ -282,9 +326,12 @@ def ac7make_time_signature(s):
   return x + 8*num
 
 
-def ac7make_element(b, addr):
-  number_of_parts = 12
-  addr = addr + 7 + 4*number_of_parts
+def ac7make_element(b, elements, start_addr):
+  number_of_parts = len(elements)
+  if number_of_parts != 12:
+    print("Expected 12 elements, got {0}".format(number_of_parts))
+    raise Exception
+  addr = start_addr + 7 + 4*number_of_parts
   
   # Magic number for "elements"
   g0 = struct.pack('<I', 0x07ffffff)
@@ -319,30 +366,98 @@ def ac7make_element(b, addr):
 
   addr = addr + len(g2)
 
-  # Set the part counters to 0, as they need to be retained throughout
-  # the loop
-  global drum_part_number
-  global other_part_number
-  global overall_part_number
-  
-  drum_part_number = 0
-  other_part_number = 0
-  overall_part_number = 0
+  # Now add the individual element definitions
 
   g1 = b''
   g3 = b''
-  for el in range(12):
-    g4 = ac7make_element_block(b, el)
-    g3 = g3 + g4
-    g1 = g1 + struct.pack('<I', addr)
-    addr = addr + len(g4)
-  g0 = g0 + struct.pack('<H', 7 + len(g1) + len(g2) + len(g3))
-  g0 = g0 + struct.pack('B', number_of_parts)
+  for ee in elements:
+    g3 += ee
+    g1 += struct.pack('<I', addr)
+    addr = addr + len(ee)
+  g0 += struct.pack('<H', 7 + len(g1) + len(g2) + len(g3))
+  g0 += struct.pack('B', number_of_parts)
   return g0 + g1 + g2 + g3
 
 
-def ac7maker(f, b):
+def ac7make_track_element(pt):
+  if pt <= 1:
+    return b'\x0f'  # 0x0F = negative 1
+  else:
+    return struct.pack('<B', pt-2)
+
+def ac7make_is_drum_part(pt):
+  return pt<=2
+
+
+def ac7maker(b):
   
+  elements = []
+  mixers = []
+  drums = []
+  others = []
+  
+  for el in range(1,13):
+    num_trk_el = 0
+    e_20 = b''
+    e_21 = b''
+    e_22 = b''
+    for pt in range(1,9):
+      num_trk = 0
+      for trk in b["rhythm"]["tracks"]:
+        if trk.get("element", -1)==el and trk.get("part", -1)==pt:
+          # Found a non-empty track to add. Add it
+          e_22 += ac7make_track_element(pt)
+    
+          if ac7make_is_drum_part(pt):
+            e_20 += struct.pack('<H', len(drums) + 0x8000)
+            drums.append(ac7make_drum_element(pt, el, b))
+          else:
+            e_20 += struct.pack('<H', len(others) + 0x8000)
+            others.append(ac7make_other_element(pt, el, b))
+          
+          if num_trk == 0:
+            # This is the first track for this element/part combo
+            e_21 += struct.pack('<H', len(mixers) + 0x8000)
+            mixers.append(ac7make_mixer_element(pt, b))
+          else:
+            # There's already a mixer for this combo. Just add a filler
+            # value
+            e_21 += struct.pack('<H', 0xFFFF)
+
+          num_trk += 1
+      if num_trk == 0:
+        # No tracks for this element/part combination. Add an empty one
+        e_22 += ac7make_track_element(pt)
+  
+        if ac7make_is_drum_part(pt):
+          e_20 += struct.pack('<H', len(drums) + 0x8000)
+          drums.append(ac7make_drum_element(pt, el, b))
+        else:
+          e_20 += struct.pack('<H', len(others) + 0x8000)
+          others.append(ac7make_other_element(pt, el, b))
+          
+        e_21 += struct.pack('<H', len(mixers) + 0x8000)
+        mixers.append(ac7make_mixer_element(pt, b))
+        
+        num_trk += 1
+        
+      num_trk_el += num_trk
+    # Have now processed all the parts & tracks for this element. Complete
+    # the element bytestring
+    el_00 = b''
+    el_00 += ac7make_element_atom(1, b'\x22')  # Time signature
+    el_00 += ac7make_element_atom(6, b'\x01')  # Number of measures
+    el_00 += ac7make_element_atom(7, struct.pack('<B', num_trk_el))  # Total number of tracks
+    el_00 += ac7make_element_atom(0x20, e_20)
+    el_00 += ac7make_element_atom(0x21, e_21)
+    el_00 += ac7make_element_atom(0x22, e_22)
+    el_00 += ac7make_element_atom(0x30, struct.pack('<8B', 0, 0, 0, 0, 0, 0, 0, 0))  # Delay send values
+    el_00 += ac7make_element_atom(253, b'')  # Start of AiX-specific data (currently none)
+    el_00 += ac7make_element_atom(254, b'')  # Start of CTX-specific data (currently none)
+    el_00 += ac7make_element_atom(255, b'')  # End
+    elements.append(el_00)
+
+
   # Size of the header. 'AC07' plus start address of each block (4 bytes),
   # plus end address (4 bytes)
   addr = 0x1C
@@ -350,21 +465,21 @@ def ac7maker(f, b):
   g1 = b''
   
   g1 += struct.pack('<I', addr)
-  g5 = ac7make_element(b, 0)
+  g5 = ac7make_element(b, elements, 0)
   addr += len(g5)
   
   g1 += struct.pack('<I', addr)
 
-  g2 = ac7make_mixer(b, addr)
+  g2 = ac7make_mixer(mixers, addr)
 
   addr += len(g2)
 
   g1 += struct.pack('<I', addr)
-  g3 = ac7make_drum(b, addr)
+  g3 = ac7make_drum(drums, addr)
   addr += len(g3)
 
   g1 += struct.pack('<I', addr)
-  g4 = ac7make_other(b, addr)
+  g4 = ac7make_other(others, addr)
   addr += len(g4)
 
   # End-of-list indicator
@@ -372,7 +487,7 @@ def ac7maker(f, b):
 
   g1 = b'AC07' + struct.pack('<I', addr) + g1
 
-  f.write(g1 + g5 + g2 + g3 + g4)
+  return g1 + g5 + g2 + g3 + g4
   
   
 # Following https://github.com/shimpe/ac7parser/Ac7CasioEventAnalyzer.py
@@ -463,27 +578,26 @@ def read_music(f):
 
 
 
-
-#for msg in mido.MidiFile('MLTREC02.MID'):
-#  print(msg)
-
-
-
+#
+# USAGE:
+#  python3 ac7maker.py <input .json file> [ <output .ac7 file> ]
+#
 
 
+if __name__=="__main__":
+  if len(sys.argv) < 2:
+    print("Returning : no input")
+    # No input, nothing to do
+  elif len(sys.argv) == 2:
+    # No output filename; send to standard out
+    with open(sys.argv[1], "r") as f1:
+      b = json.load(f1)
+    sys.stdout.write(ac7maker(b))
+  else:
+    # Has an output filename. Write to it.
+    with open(sys.argv[1], "r") as f1:
+      b = json.load(f1)
+    with open(sys.argv[2], "wb") as f2:
+      f2.write(ac7maker(b))
+      b = json.load(f1)
 
-
-
-
-#with open("pt2el3.txt", "r") as f3:
-#  c = read_music(f3)
-#  print(c)
-
-
-with open("example.json", "r") as f1:
-  b = json.load(f1)
-  print(b)
-  print(b['target_model'])
-  
-with open("aa.AC7", "wb") as f2:
-  ac7maker(f2, b)
